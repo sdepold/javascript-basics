a4196ce132b1b2c946f0445cfbdeacce7346dffb
Sascha Depold <sdepold@ebay.com>
2019-09-02 09:24:08 +0200

    Add skeleton

---
+++ misc/01-cookies/README.md
@@ -0,0 +1,12 @@
+# Misc 01 | Cookies
+
+Outline
+
+- Build web app where people can comment on something
+- Unsent comments shall remain available even after refreshing the page
+
+## Steps
+
+1. Run the app and check how it works.
+2. Review handle-form.js
+3. Extend it to store all type input in the cookie so that it is used after a refresh
---
+++ misc/01-cookies/handle-form.js
@@ -0,0 +1,44 @@
+const jsonId = "10ryor";
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  fetch(`https://api.myjson.com/bins/${jsonId}`)
+    .then(res => res.json())
+    .then(data => {
+      data.comments = data.comments || [];
+      data.comments.push({
+        username: document.querySelector("[name=username]").value,
+        message: document.querySelector("[name=message]").value
+      });
+      return data;
+    })
+    .then(data =>
+      fetch(`https://api.myjson.com/bins/${jsonId}`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+    ).then(()=>window.location.reload());
+});
+
+fetch(`https://api.myjson.com/bins/${jsonId}`)
+  .then(res => res.json())
+  .then(data => {
+    const commentsDiv = document.querySelector("#comments");
+
+    commentsDiv.innerHTML="";
+
+    (data.comments || []).forEach(comment => {
+      const div = document.createElement("div");
+      const content = document.createTextNode(
+        `${comment.username}: ${comment.message}`
+      );
+
+      div.appendChild(content);
+      commentsDiv.appendChild(div);
+    });
+
+});
